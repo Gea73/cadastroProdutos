7a40e2049d89460c4e5d596af80107585f14e531
Gea73 <geanccarlos2006@gmail.com>
2025-11-24 17:13:32 -0300

    final

--- app.js
+++ app.js
@@ -1,103 +1,229 @@
-
+//abre o uma requisição pro banco de dados
+let db;
 const request = indexedDB.open("ProdutosDB", 3);
 
+let editando = false;
+let idEdit = null;
 
-request.onupgradeneeded = event => {
-    const db = event.target.result;
+request.onupgradeneeded = (event) => {
+  db = event.target.result;
 
-    if (!db.objectStoreNames.contains("produtos")) {
-        db.createObjectStore("produtos", { keyPath: "id", autoIncrement: "true" });
-    }
-    console.log("Banco atualizado com sucesso");
+  if (!db.objectStoreNames.contains("produtos")) {
+    const objectStore = db.createObjectStore("produtos", {
+      keyPath: "id",
+      autoIncrement: "true",
+    });
 
-}
-
-
-request.onerror = event => {
-    console.error("Erro no banco", + event.target.errorCode);
+    objectStore.createIndex("nome", "nome", { unique: true });
+    objectStore.createIndex("preco", "preco", { unique: false });
+    objectStore.createIndex("categoria", "categoria", { unique: false });
+  }
+  console.log("Banco atualizado com sucesso");
 };
 
-let db = null;
-
-request.onsuccess = event => {
-    db = event.target.result;
-    console.log("Banco conectado com sucesso");
-    AtualizarTabela();
+request.onerror = (event) => {
+  console.error("Erro no banco" + event.target.error?.message);
 };
 
-
+request.onsuccess = (event) => {
+  db = event.target.result;
+  console.log("Banco conectado com sucesso");
+  AtualizarTabela();
+};
 
 function SalvarProduto() {
-    const msg = document.querySelector('.msg');
-    msg.innerHTML = '';
-    const nomeValor = document.querySelector("#nome-produto").value.trim();
-    const precoValor = document.querySelector("#preco-produto").value;
-    const categoriaValor = document.querySelector("#categoria").value;
-    if (!nomeValor || !precoValor || !categoriaValor) {
-        console.log("Campos vazios")
-        msg.innerHTML = 'Campo vazio preencha todos os campos!';
-        return;
-    }
-    const tran = db.transaction("produtos", "readwrite");
-    const store = tran.objectStore("produtos");
+  const msg = document.querySelector(".msg");
+  msg.innerHTML = "";
 
-    const novoProduto = { nome: nomeValor, preco: Number(precoValor), categoria: categoriaValor };
+  const nomeValor = document.querySelector("#nome-produto").value.trim();
+  const precoValor = document.querySelector("#preco-produto").value;
+  const categoriaValor = document.querySelector("#categoria").value;
+
+  if (!nomeValor || !precoValor || !categoriaValor) {
+    console.log("Campos vazios");
+    msg.innerHTML = "Campo vazio preencha todos os campos!";
+    return;
+  }
+
+  const transacao = db.transaction("produtos", "readwrite");
+  const store = transacao.objectStore("produtos");
+
+  if (!editando) {
+    const novoProduto = {
+      nome: nomeValor,
+      preco: Number(precoValor),
+      categoria: categoriaValor,
+    };
 
     store.add(novoProduto);
 
-    tran.oncomplete = () => {
-        console.log("Produto adicionado:", novoProduto);
-        msg.innerHTML = 'Produto adicionado!';
-        AtualizarTabela();
+    transacao.oncomplete = () => {
+      console.log("Produto adicionado:", novoProduto);
+      msg.innerHTML = "Produto adicionado!";
+      AtualizarTabela();
+      const form = document.querySelector("form");
+      form.reset();
     };
 
-    tran.onerror = () => {
-        console.error("Erro ao adicionar produto");
-        msg.innerHTML = 'Erro ao adicionar produto!';
+    transacao.onerror = () => {
+      console.error("Erro ao adicionar produto");
+      msg.innerHTML = "Erro ao adicionar produto!";
+    };
+  } else {
+    const index = store.index("nome");
+    const request = index.get(nomeValor);
+
+    request.onsuccess = () => {
+      const produto = request.result;
+
+      EditarProdutoBanco(idEdit, nomeValor, precoValor, categoriaValor, msg);
+    };
+  }
+}
+
+function EditarProduto(id) {
+  editando = true;
+  idEdit = id;
+  const nomeInput = document.querySelector("#nome-produto");
+  const precoInput = document.querySelector("#preco-produto");
+  const categoriaInput = document.querySelector("#categoria");
+
+  const transacao = db.transaction("produtos", "readwrite");
+  const store = transacao.objectStore("produtos");
+  const request = store.get(id);
+  request.onerror = (event) => {
+    console.error("Erro ao recuperar produto" + event.target.error?.message);
+  };
+  request.onsuccess = (event) => {
+    const produto = event.target.result;
+
+    nomeInput.value = produto.nome;
+    precoInput.value = produto.preco;
+    categoriaInput.value = produto.categoria;
+  };
+}
+
+function EditarProdutoBanco(id, nome, preco, categoria, msg) {
+  const transacao = db.transaction("produtos", "readwrite");
+  const store = transacao.objectStore("produtos");
+  const request = store.get(id);
+
+  request.onerror = (event) => {
+    console.error("Erro ao recuperar produto" + event.target.error?.message);
+  };
+  request.onsuccess = (event) => {
+    const produto = event.target.result;
+
+    produto.nome = nome;
+    produto.preco = preco;
+    produto.categoria = categoria;
+
+    const requestUpdate = store.put(produto);
+
+    requestUpdate.onerror = (event) => {
+      console.error("Erro ao atualizar produto" + event.target.error?.message);
+      msg.innerHTML = "Erro ao Atualizar!";
     };
 
+    requestUpdate.onsuccess = () => {
+      console.log("Produto atualizado:", produto);
+      msg.innerHTML = "Produto Atualizado!";
+    };
+  };
+
+  transacao.oncomplete = () => {
+    AtualizarTabela();
+    editando = false;
+    const form = document.querySelector("form");
+    form.reset();
+  };
+}
+
+function DeletarProduto(id) {
+  const transacao = db.transaction("produtos", "readwrite");
+  const store = transacao.objectStore("produtos");
+  const request = store.delete(id);
+
+  request.onerror = (event) => {
+    console.error("Erro ao recuperar produto" + event.target.error?.message);
+  };
+  request.onsuccess = () => {
+    console.log("Deletado");
+  };
+  AtualizarTabela();
+}
+
+function DeletarDB() {
+  const request = indexedDB.deleteDatabase("ProdutosDB");
+
+  request.onsuccess = () => {
+    console.log("Banco deletado com sucesso!");
+    location.reload();
+  };
+
+  request.onerror = (event) => {
+    console.error("Erro ao deletar banco:", event.target.error);
+  };
+
+  request.onblocked = () => {
+    location.reload();
+  };
 }
 
 function AtualizarTabela() {
-    const tbody = document.querySelector('.table-body');
-    tbody.innerHTML = '';
+  const tbody = document.querySelector(".table-body");
+  tbody.innerHTML = "";
 
-    const tran = db.transaction("produtos", "readonly");
-    const store = tran.objectStore("produtos");
-    const request = store.openCursor();
+  const transacao = db.transaction("produtos", "readonly");
+  const store = transacao.objectStore("produtos");
+  const request = store.openCursor();
 
-    request.onsuccess = (event) => {
-        const cursor = event.target.result;
+  request.onsuccess = (event) => {
+    const cursor = event.target.result;
 
-        if (cursor) {
-            const produto = cursor.value;
+    if (cursor) {
+      const produto = cursor.value;
 
-            const tr = document.createElement('tr');
-            const tdId = document.createElement('td');
-            tdId.textContent = produto.id;
-            const tdNome = document.createElement('td');
-            tdNome.textContent = produto.nome;
-            const tdPreco = document.createElement('td');
-            tdPreco.textContent = `R$ ${Number(produto.preco).toFixed(2)}`;
-            const tdCat = document.createElement('td');
-            tdCat.textContent = produto.categoria;
+      const tr = document.createElement("tr");
+      const tdId = document.createElement("td");
+      tdId.textContent = produto.id;
+      const tdNome = document.createElement("td");
+      tdNome.textContent = produto.nome;
+      const tdPreco = document.createElement("td");
+      tdPreco.textContent = `R$ ${Number(produto.preco).toFixed(2)}`;
+      const tdCat = document.createElement("td");
+      tdCat.textContent = produto.categoria;
 
-            tr.appendChild(tdId);
-            tr.appendChild(tdNome);
-            tr.appendChild(tdPreco);
-            tr.appendChild(tdCat);
-            tbody.appendChild(tr);
+      const editBtn = document.createElement("button");
+      editBtn.textContent = "Editar";
 
-            cursor.continue();
-        } else {
+      editBtn.classList.add("btn-editar");
+      editBtn.onclick = function () {
+        EditarProduto(produto.id);
+      };
 
-            console.log("Fim dos produyos");
-        }
-    };
+      const deleteBtn = document.createElement("button");
+      deleteBtn.textContent = "Deletar";
+      deleteBtn.classList.add("btn-deletar");
+      deleteBtn.onclick = function () {
+        DeletarProduto(produto.id);
+      };
 
-    request.onerror = (event) => {
-        console.error("Erro no cursor", event);
-    };
+      tr.appendChild(tdId);
+      tr.appendChild(tdNome);
+      tr.appendChild(tdPreco);
+      tr.appendChild(tdCat);
+      tr.appendChild(editBtn);
+      tr.appendChild(deleteBtn);
+      tbody.appendChild(tr);
 
+      cursor.continue();
+    } else {
+      console.log("Fim dos produyos");
+    }
+  };
 
+  request.onerror = (event) => {
+    console.error("Erro no cursor", event);
+  };
 }
